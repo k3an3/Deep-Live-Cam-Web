37b42e7306b21ca044997bcab8d6b6eed6ed2024
Keane O'Kelley <me@keane.space>
2024-08-18 12:44:11 -0400

    A number of fixes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import argparse
 import asyncio
+import os.path
 from uuid import uuid4
 
 import aiofiles.os
-import asynctempfile
+import aiofiles.tempfile
 from fastapi import FastAPI, File, UploadFile, BackgroundTasks
+from starlette.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -13,9 +15,15 @@ app.mount("/output", StaticFiles(directory="output"), name="output")
 tasks = {}
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--python-path", default="python3", help="Path to python interpreter")
-parser.add_argument("--run-py-path", default="run.py", help="Path to the python script to be run")
-parser.add_argument("--num-threads", type=int, default=60, help="Number of execution threads")
+parser.add_argument(
+    "--python-path", default="python3", help="Path to python interpreter"
+)
+parser.add_argument(
+    "--run-py-path", default="run.py", help="Path to the python script to be run"
+)
+parser.add_argument(
+    "--num-threads", type=int, default=60, help="Number of execution threads"
+)
 parser.add_argument("--provider", default="cuda", help="Execution provider")
 args = parser.parse_args()
 
@@ -25,47 +33,62 @@ EXECUTION_THREADS = args.num_threads
 EXECUTION_PROVIDER = args.provider
 
 
-async def do_face_replacement(task_id: str, face_image_path: str, media_path: str, output_filename: str):
-    print(f"echo 'Processing {face_image_path} on {media_path} and saving to {output_filename}'")
-    process = await asyncio.create_subprocess_exec(PYTHON,
-                                                   [RUN_PY, "--execution-threads", EXECUTION_THREADS,
-                                                    "--execution-provider",
-                                                    EXECUTION_PROVIDER, "--source", face_image_path, "--target",
-                                                    media_path, "--output",
-                                                    output_filename])
-    tasks[task_id]['status'] = 'processing'
+async def do_face_replacement(
+        task_id: str, face_image_path: str, media_path: str, output_filename: str
+):
+    print(
+        f"Processing {face_image_path} on {media_path} and saving to {output_filename}"
+    )
+    process = await asyncio.create_subprocess_exec(
+        PYTHON,
+        RUN_PY,
+        "--execution-threads",
+        str(EXECUTION_THREADS),
+        "--execution-provider",
+        EXECUTION_PROVIDER,
+        "--source",
+        face_image_path,
+        "--target",
+        media_path,
+        "--output",
+        output_filename,
+    )
+    tasks[task_id]["status"] = "processing"
     await process.wait()
-    tasks[task_id]['status'] = 'done'
-    await asyncio.gather(aiofiles.os.unlink(face_image_path), aiofiles.os.unlink(media_path))
+    tasks[task_id]["status"] = "done"
+    await asyncio.gather(
+        aiofiles.os.unlink(face_image_path), aiofiles.os.unlink(media_path)
+    )
+    print("Completed task", task_id)
 
 
 @app.post("/process")
-async def process_request(background_tasks: BackgroundTasks,
-                          face_image: UploadFile = File(...),
-                          media_file: UploadFile = File(...)):
+async def process_request(
+        background_tasks: BackgroundTasks,
+        face_image: UploadFile = File(...),
+        media_file: UploadFile = File(...),
+):
     # Generate a unique task ID and output filename
     task_id = str(uuid4())
-    if 'mp4' in media_file.content_type:
-        output_filename = f"output/{task_id}.mp4"  # Assume the output is a video file
-    else:
-        output_filename = f"output/{task_id}.jpg"
+    extension = os.path.splitext(media_file.filename)[1]
+    face_extension = os.path.splitext(face_image.filename)[1]
+    output_filename = f"output/{task_id}{extension}"  # Assume the output is a video file
 
-    with asynctempfile.NamedTemporaryFile(delete=False) as f:
+    async with aiofiles.tempfile.NamedTemporaryFile("wb", suffix=face_extension, delete=False) as f:
         await f.write(await face_image.read())
         face_image_path = f.name
 
-    with asynctempfile.NamedTemporaryFile(delete=False) as f:
+    async with aiofiles.tempfile.NamedTemporaryFile("wb", suffix=extension, delete=False) as f:
         await f.write(await media_file.read())
         media_path = f.name
 
     # Store task details
-    tasks[task_id] = {
-        "status": "processing",
-        "output_file": output_filename
-    }
+    tasks[task_id] = {"status": "processing", "output_file": output_filename}
 
     # Run the system command in the background
-    background_tasks.add_task(do_face_replacement, task_id, face_image_path, media_path, output_filename)
+    background_tasks.add_task(
+        do_face_replacement, task_id, face_image_path, media_path, output_filename
+    )
 
     # Return the task ID immediately
     return {"task_id": task_id}
@@ -79,10 +102,9 @@ def task_status(task_id: str):
         return {"error": "Task not found"}
 
 
-@app.get("/tasks/")
-def task_status(task_id: str):
-    return tasks
-
+@app.get("/")
+async def read_index():
+    return FileResponse('static/index.html')
 
 if __name__ == "__main__":
     import uvicorn
